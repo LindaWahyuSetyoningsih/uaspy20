03f0b2dfe3b2df11a8555228510411b261ec929a
Linda Wahyu Setyoningsih <56390857+LindaWahyuSetyoningsih@users.noreply.github.com>
2020-01-18 18:07:55 +0700

    Update author.py

--- data_model/author.py
+++ data_model/author.py
@@ -1,9 +1,9 @@
 class Author:
     def __init__(self, name=""):
-        self.name = name
-        self.gender = ""
-        self.address = ""
-        self.phone = 0
+        self.name = "Linda Wahyu Setyoningsih"
+        self.gender = "Perempuan"
+        self.address = "Cibitung"
+        self.phone = "0895326388827"
 
     def toString(self):
         return "Name: {0}, Address: {1}".format(self.name, self.address)
